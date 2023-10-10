50ef2a579c6feaa8f7f5cfa7515eedf1007d650d
NoyonPolygon <nuruzzaman@polygontech.xyz>
2023-10-10 16:11:59 +0600

    first commit

---
+++ components/CustomButton.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { CustomButtonProps } from "@/types";
+import Image from "next/image";
+
+const CustomButton = ({title, containerStyles, handleClick} : CustomButtonProps) => {
+  return (
+    <button
+        disabled={false}
+        type={"button"}
+        className={`custom-btn ${containerStyles}`}
+        onClick={()=>{}}
+    >
+        {title}
+    </button>
+  )
+}  
+
+export default CustomButton
