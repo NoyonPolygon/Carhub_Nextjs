c4fff9b60f3390ae9ac1de33dc112000aaad4bd1
NoyonPolygon <nuruzzaman@polygontech.xyz>
2023-10-10 19:17:27 +0600

    add navbar footer searchbar

---
+++ components/SearchBar.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+const SearchBar = () => {
+
+    const handleSearch = () => {
+
+    }
+
+  return (
+    // <div>SearchBar</div>
+    <form className='searchbar' onSubmit={handleSearch}>
+
+    </form>
+  )
+}
+
+export default SearchBar
--- types/index.ts
+++ types/index.ts
@@ -3,5 +3,6 @@ import { MouseEventHandler } from "react";
 export interface CustomButtonProps{
     title: String;
     containerStyles?: String;
+    btnType?: "button" | "submit";
     handleClick?: MouseEventHandler<HTMLButtonElement>;
 }
